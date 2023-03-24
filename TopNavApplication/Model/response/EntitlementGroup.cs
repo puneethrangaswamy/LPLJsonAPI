6f1e8c05112b2bef96e0d279851bee473f79170e
Puneeth <puneeth.rangaswamy@incedoinc.com>
2023-03-24 17:54:26 -0500

    Missed file addition

---
+++ TopNavApplication/Model/response/EntitlementGroup.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+
+namespace TopNavApplication.Model.response
+{
+    public class EntitlementGroup
+    {
+        public int ID { get; set; }
+        public string name;
+        public string description;
+
+        public static string GetRoleByUserName(NpgsqlDataReader rs)
+        {
+            EntitlementGroup eg = null;
+            while (rs.Read())
+            {
+                eg = new EntitlementGroup();
+                if (rs[0] != DBNull.Value)
+                    eg.ID = (int)rs[0];
+
+                if (rs[1] != DBNull.Value)
+                    eg.name = rs[1].ToString();
+
+                if (rs[2] != DBNull.Value)
+                    eg.description = rs[2].ToString();
+
+            }
+            return eg.name;
+        }
+
+
+    }
+
+  
+}
